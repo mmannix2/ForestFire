33e07cacdd45f48e11c1d215e21c2713cb787cb1
Matthew Mannix <m.mannix3@gmail.com>
2016-10-26 10:08:12 -0400

    Initial commit. Add forestfire.c and makefile.

---
+++ forestfire.c
@@ -0,0 +1,9 @@
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    
+    //TODO Write the program.
+    
+    MPI_Finalize();
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+CC=mpicc
+CFLAGS=-Wall -g --std=c99
+TARGET=forestfire
+
+all: $(TARGET)
+
+$(TARGET):
+	$(CC) $(CFLAGS) $(TARGET).c -o $(TARGET)
