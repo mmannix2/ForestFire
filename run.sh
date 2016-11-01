557ade7789c7b9bf499e7e16473ed12fb9ba14eb
Matthew Mannix <m.mannix3@gmail.com>
2016-11-01 13:40:25 -0400

    Make run.sh work with a variety of input.

--- run.sh
+++ run.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 #A script to run the forest fire simulation
 
-if [ $# -ne 5  ]
-then
+exe=forestfire
+infile=test/topfire.txt
+N=1000
+I=0.001
+G=0.001
+
+case $# in
+5)
+    exe=$1
+    infile=$2
+    N=$3
+    I=$4
+    G=$5
+    ;;
+4)
+    infile=$1
+    N=$2
+    I=$3
+    G=$4
+    ;;
+3)
+    N=$1
+    I=$2
+    G=$3
+    ;;
+1)
+    infile=$1
+    ;;
+0)
+    ;;
+*)
     echo "Bad input."
     exit 1
-else
-    mpirun -np 8 $1 $2 $3 $4 $5
-    exit 0
-fi
+esac
+
+mpirun -np 8 $exe $infile $N $I $G
