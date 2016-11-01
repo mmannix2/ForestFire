c1cd4305ca6b14d187905628754015ee5f051623
Matthew Mannix <m.mannix3@gmail.com>
2016-11-01 13:43:06 -0400

    Add comments to run.sh.

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ G=0.001
 
 case $# in
 5)
+    #Specify all of the parameters
     exe=$1
     infile=$2
     N=$3
@@ -16,24 +17,30 @@ case $# in
     G=$5
     ;;
 4)
+    #Use default exe
     infile=$1
     N=$2
     I=$3
     G=$4
     ;;
 3)
+    #Use default exe and infile
     N=$1
     I=$2
     G=$3
     ;;
 1)
+    #Only specify the infile
     infile=$1
     ;;
 0)
+    #Use all defaults
     ;;
 *)
     echo "Bad input."
     exit 1
 esac
 
+#Run this baby!
 mpirun -np 8 $exe $infile $N $I $G
+exit $?
