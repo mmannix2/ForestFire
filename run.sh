35b618b98c19dd4f77feba11a50601066a5eb5f7
Matthew Mannix <m.mannix3@gmail.com>
2016-11-01 13:28:23 -0400

    Start work on run.sh.

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-#A script to run the forest fire simulation 
+#A script to run the forest fire simulation
+
+if [ $# -ne 5  ]
+then
+    echo "Bad input."
+    exit 1
+else
+    mpirun -np 8 $1 $2 $3 $4 $5
+    exit 0
+fi
